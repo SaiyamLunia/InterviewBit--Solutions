f4a033a8c8b36aa47d5eba1dadbb58b063cde667
Saiyam Lunia <40720715+SaiyamLunia@users.noreply.github.com>
2020-07-04 18:56:57 +0530

    solutions added for StacksAndQueues

---
+++ StacksAndQueues/Evaluate Expression.java
@@ -0,0 +1,58 @@
+/*
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+Input Format
+The only argument given is character array A.
+Output Format
+Return the value of arithmetic expression formed using reverse Polish Notation.
+For Example
+
+Input 1:
+    A =   ["2", "1", "+", "3", "*"]
+Output 1:
+    9
+Explaination 1:
+    starting from backside:
+    *: ( )*( )
+    3: ()*(3)
+    +: ( () + () )*(3)
+    1: ( () + (1) )*(3)
+    2: ( (2) + (1) )*(3)
+    ((2)+(1))*(3) = 9
+    
+Input 2:
+    A = ["4", "13", "5", "/", "+"]
+Output 2:
+    6
+Explaination 2:
+    +: ()+()
+    /: ()+(() / ())
+    5: ()+(() / (5))
+    1: ()+((13) / (5))
+    4: (4)+((13) / (5))
+    (4)+((13) / (5)) = 6
+*/
+
+public class Solution {
+    public int evalRPN(ArrayList<String> A) {
+        Stack<Integer> stack = new Stack<>();
+        String operators = "+-*/";
+        for(String s: A) {
+            if(operators.contains(s)) {
+                int x = stack.pop();
+                int y = stack.pop();
+                if(s.equals("+"))
+                    stack.push(x + y);
+                else if(s.equals("-"))
+                    stack.push(y - x);
+                else if(s.equals("*"))
+                    stack.push(x * y);
+                else if(s.equals("/"))
+                    stack.push(y / x);
+            }
+            else
+                stack.push(Integer.parseInt(s));
+        }
+        return stack.peek();
+    }
+}
